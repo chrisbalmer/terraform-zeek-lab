1dffbd970154df14416930abe05f7b518c44f0bb
Chris Balmer <git@chrisbalmer.io>
2020-04-26 20:55:47 -0400

    Moved backend to local MinIO.

--- main.tf
+++ main.tf
@@ -22,6 +22,20 @@ terraform {
       version = "1.11.0"
     }
   }
+
+  backend "s3" {
+    bucket                      = "terraform"
+    key                         = "labs/zeek"
+    region                      = "us-east-1"
+    endpoint                    = "https://nas.balmerfamilyfarm.com:9000"
+    profile                     = "nas"
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style            = true
+  }
 }
 
 provider "onepassword" {
